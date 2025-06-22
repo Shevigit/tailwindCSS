f9cbf06fbc6aa0e1950eeee026727b1f26534d1f
Tamar <tamarmmi123@gmail.com>
2025-06-22 16:38:47 +0300

    sidebar tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./pages/SidebarContainer";
 import Home from "./pages/Home";
+import SidebarContainer from "./pages/SidebarContainer";
 
 
 export default function App() {
   return (
     <div>
+      <SidebarContainer />
  <Home></Home>
     </div>
   );
